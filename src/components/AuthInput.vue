378729b0b79292379f53233e05e138f468a4b3f8
itcast-hsian <541510140@qq.com>
2019-09-22 10:54:08 +0800

    输入框双向数据绑定

---
+++ src/components/AuthInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- @input是输入框每次输入时候都会触发 -->
+  <input 
+    class="input"
+    :placeholder="placeholder"
+    :value="value"
+    @input="handleInput"
+  />
+</template>
+
+<script>
+export default {
+    props:[
+        "placeholder",
+        "value",
+        "name",
+
+        "rule",
+    ],
+
+    mounted(){
+        // console.log(this.rule)
+    },
+
+    methods: {
+        // 每次输入框输入值时候都会触发
+        handleInput(event){
+            this.$emit("input", event.target.value)
+        }
+    }
+}
+</script>
+
+<style scoped lang="less">
+.input{
+    width:100%;
+    height: 38 / 360 * 100vw;
+    padding:20px;
+    box-sizing: border-box;
+    background: #fff;
+    border:none;
+    border-bottom: 1px #666 solid;
+    outline: none;
+}
+</style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
       <!-- 关闭的按钮 -->
       <div class="close">
         <span class="iconfont iconicon-test"></span>
@@ -11,33 +11,65 @@
       </div>
 
       <!-- 用户名密码输入框 -->
-      <div>
-        <input type="text">
-      </div>
-      <div>
-        <input type="text">
+      <div >
+
+        <!-- 输入框组件 -->
+        <AuthInput
+        placeholder="手机号码"
+        
+        :value='form.username'
+        @input="handleUsername"
+        ></AuthInput>
+
       </div>
 
-      登录按钮
-      <button></button>
+      
+      <button @click="handleSubmit">登录按钮</button>
   </div>
 </template>
 
 <script>
+
+//  导入输入框组件
+import AuthInput from "@/components/AuthInput";
+
 export default {
   data(){
     return {
-      list: []
+      // 发送给后台的数据
+      form: {
+        username: "",
+        password: ""
+      }
+    }
+  },
+  // 注册组件
+  components: {
+    AuthInput
+  },
+
+  methods: {
+    // 传递给输入框组件，用于获取用户名
+    handleUsername(value){
+      this.form.username = value;
+    },
+    // 表单提交
+    handleSubmit(){
+      console.log(this.form)
     }
   }
 }
 </script>
 
-<style lang="less">
+<style scoped lang="less">
+  // scoped 作用域样式
   // lang声明样式的类型
-  .close{
-    padding:20px;
 
+  .container{
+    padding:20px;
+  }
+
+  .close{
     span{
       font-size: 27 / 360 * 100vw;
     }
