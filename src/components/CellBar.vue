0789502b3638974afd955aab3670dcba5712d6e8
itcast-hsian <541510140@qq.com>
2019-09-24 10:15:15 +0800

    调用条形组件

--- src/components/CellBar.vue
+++ src/components/CellBar.vue
@@ -1,39 +1,41 @@
 <template>
-  <div class="cell-wrap">
-      <div class="cell">
-          <span>我的关注</span>
-          <i>关注的用户</i>
-          <span class="iconfont iconjiantou1"></span>
-      </div>
+  <!-- $event是模板事件对象 -->
+  <div class="cell-wrap" @click="$emit('click', $event)">
+    <div class="cell">
+      <span>{{label}}</span>
+      <i>{{text}}</i>
+      <span class="iconfont iconjiantou1"></span>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  // label左侧的文件， text是右侧的文字
+  props: ["label", "text"]
+};
 </script>
 
 <style scoped lang="less">
-    .cell-wrap{
-        padding-left:10px;
+.cell-wrap {
+  padding-left: 10px;
 
-        .cell{
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            padding-right: 10px;
-            height:48 / 360 * 100vw;
-            border-bottom: 1px #eee solid;
+  .cell {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-right: 10px;
+    height: 48 / 360 * 100vw;
+    border-bottom: 1px #eee solid;
 
-            i{
-                display: block;
-                flex:1;
-                text-align: right;
-                padding:0 10px;
-                font-size:14px;
-                color:#999;
-            }
-        }
+    i {
+      display: block;
+      flex: 1;
+      text-align: right;
+      padding: 0 10px;
+      font-size: 14px;
+      color: #999;
     }
+  }
+}
 </style>
--- src/pages/Personal.vue
+++ src/pages/Personal.vue
@@ -17,7 +17,13 @@
         </div>
 
         <!-- 调用条形组件 -->
-        <CellBar/>
+        <CellBar label="我的关注" text="关注的用户"/>
+
+        <CellBar label="我的跟帖" text="跟帖/回复"/>
+
+        <CellBar label="我的收藏" text="文章/视频"/>
+
+        <CellBar label="设置"/>
   </div>
 </template>
 
