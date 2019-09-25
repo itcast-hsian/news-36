ddc322dfa09c37cc2418b4fbabe18a6aeac275c1
itcast-hsian <541510140@qq.com>
2019-09-25 09:45:10 +0800

    资料页静态布局

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -1,22 +1,47 @@
 <template>
   <div>
-      <HeaderNormal title="编辑资料"/>
+    <!-- 头部 -->
+    <HeaderNormal title="编辑资料" />
 
-      
+    <!-- 头像 -->
+    <div class="head">
+      <img src="../../static/default_green.jpg" alt />
+    </div>
+
+    <!-- 调用条形组件 -->
+    <CellBar label="昵称" text="火星网友" />
+
+    <CellBar label="密码" text="123" />
+
+    <CellBar label="性别" text="男" />
   </div>
 </template>
 
 <script>
 // 导入头部
-import HeaderNormal from "@/components/HeaderNormal"
+import HeaderNormal from "@/components/HeaderNormal";
+import CellBar from "@/components/CellBar";
 
 export default {
-    components: {
-        HeaderNormal
-    }
-}
+  components: {
+    HeaderNormal,
+    CellBar
+  }
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.head{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding:20px;
 
+    img{
+        display: block;
+        width: 100 / 360 * 100vw;
+        height: 100 / 360 * 100vw;
+        border-radius: 50%;
+    }
+}
 </style>
