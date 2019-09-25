f4c71a9bb2eac6e8c42793c40619f85564edcbe3
itcast-hsian <541510140@qq.com>
2019-09-25 10:46:57 +0800

    上传图片到服务器

--- src/pages/EditProfile.vue
+++ src/pages/EditProfile.vue
@@ -5,16 +5,16 @@
 
     <!-- 头像 -->
     <div class="head">
-        <img :src="profile.head_img" alt />
+      <img :src="profile.head_img" alt />
 
-        <!-- vant上传组件 -->
-        <van-uploader :after-read="afterRead" class="uploader"/>
+      <!-- vant上传组件 -->
+      <van-uploader :after-read="afterRead" class="uploader" />
     </div>
 
     <!-- 调用条形组件 -->
     <CellBar label="昵称" :text="profile.nickname" />
 
-    <CellBar label="密码" :text="profile.password" type="password"/>
+    <CellBar label="密码" :text="profile.password" type="password" />
 
     <CellBar label="性别" :text="profile.gender === 1 ? '男' : '女'" />
   </div>
@@ -26,17 +26,42 @@ import HeaderNormal from "@/components/HeaderNormal";
 import CellBar from "@/components/CellBar";
 
 export default {
-
-  data(){
-      return {
-          // 用户详情
-          profile: {}
-      }
+  data() {
+    return {
+      // 用户详情
+      profile: {}
+    };
   },
 
   components: {
     HeaderNormal,
     CellBar
+  },
+
+  methods: {
+    // 选择完图片之后的回调函数,file返回选中的图片
+    afterRead(file) {
+
+        //构造表单数据
+        const formData = new FormData();
+        // 通过表单使用append方法追加数据
+        formData.append('file', file.file);
+
+        this.$axios({
+            url: "/upload",
+            method: 'POST',
+            // 添加头信息
+            headers: {
+                Authorization: localStorage.getItem("token")
+            },
+            data: formData
+        }).then(res => {
+            const {data} = res.data;
+
+            // 替换用户资料的头像
+            this.profile.head_img = this.$axios.defaults.baseURL + data.url;
+        })
+    }
   },
 
   mounted() {
@@ -74,9 +99,9 @@ export default {
   padding: 20px;
   position: relative;
 
-  .uploader{
-      position: absolute;
-      opacity: .8;
+  .uploader {
+    position: absolute;
+    opacity: 0.8;
   }
 
   img {
@@ -88,8 +113,8 @@ export default {
 }
 
 // 如果要修改第三方组件库的样式时候，需要在前面加上/deep/， 因为组件库的样式不受scoped的影响
-/deep/ .van-uploader__upload{
-    width: 100 / 360 * 100vw;
-    height: 100 / 360 * 100vw;
+/deep/ .van-uploader__upload {
+  width: 100 / 360 * 100vw;
+  height: 100 / 360 * 100vw;
 }
 </style>
