79b89701e8d8f9364318f4148024717b8de75c4b
itcast-hsian <541510140@qq.com>
2019-09-26 11:37:37 +0800

    多张图片的文章列表布局

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="card">
-      <!-- 左侧文字 -->
-      <div class="card-left">
-          <div class="post-title">
-              {{post.title}}
-          </div>
-          <p class="post-info">
-              <span>{{post.user.nickname}}</span>
-              <span>{{post.comment_length}}跟帖</span>
-          </p>
-      </div>
+  <div>
+    <!-- 单张图片显示的布局 -->
+    <div class="card" v-if="post.cover.length > 0 && post.cover.length < 3">
+        <!-- 左侧文字 -->
+        <div class="card-left">
+            <div class="post-title">
+                {{post.title}}
+            </div>
+            <p class="post-info">
+                <span>{{post.user.nickname}}</span>
+                <span>{{post.comment_length}}跟帖</span>
+            </p>
+        </div>
 
-      <div class="card-img">
-          <img :src="post.cover[0].url" alt="">
-      </div>
+        <div class="card-img">
+            <img :src="post.cover[0].url" alt="">
+        </div>
+    </div>
+
+    <!-- 3张图片显示的布局 -->
+    <div class="img-cart" v-if="post.cover.length >= 3">
+        <div class="post-title">
+            {{post.title}}
+        </div>
+        <div class="img-list">
+            <img 
+            v-for="(item, index) in post.cover" 
+            :key="index"
+            :src="item.url" 
+            v-if="index < 3"
+            >
+        </div>
+        <p class="post-info">
+                <span>{{post.user.nickname}}</span>
+                <span>{{post.comment_length}}跟帖</span>
+            </p>
+    </div>
   </div>
 </template>
 
@@ -61,6 +83,39 @@ export default {
                 height: 80 / 360 * 100vw;
                 object-fit: cover;
             }
+        }
+    }
+
+    .img-cart{
+        padding: 20px 10px;
+        border-bottom: 1px #ccc solid;
+
+        .post-title{
+            font-size: 14px;
+            line-height: 1.5;
+            margin-bottom: 5px;
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: 2;
+            overflow: hidden;
+        }
+
+        .img-list{
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 5px;
+
+            img{
+                display: block;
+                width: 33%;
+                height: 80 / 360 * 100vw;
+                object-fit: cover;
+            }
+        }
+
+        .post-info{
+            font-size:12px;
+            color:#999;
         }
     }
 </style>
