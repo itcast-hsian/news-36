e6074259d62adc78c85f2220ffdd0558ea5dabc8
itcast-hsian <541510140@qq.com>
2019-09-26 09:47:36 +0800

    tab栏布局

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,12 +15,31 @@
           </router-link>
       </div>
 
+    <!-- 栏目列表 -->
+    <!-- v-model: 默认选中的标签栏 -->
+    <!-- sticky: 配置粘性布局 -->
+    <!-- swipeable: 滑动切换 -->
+    <van-tabs v-model="active" sticky swipeable>
+        <!-- title: 要显示的内容 -->
+        <van-tab 
+        v-for="index in 8" 
+        :key="index"
+        :title="'标签 ' + index"
+        >
+            <p v-for="index in 100">内容 {{ index }} </p>
+        </van-tab>
+    </van-tabs>
+
   </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return {
+            active: 1
+        }
+    }
 }
 </script>
 
